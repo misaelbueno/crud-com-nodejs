673333554923d26b33b56ad64864fd61e8ee87aa
misael.bueno <misael.bueno@meta.com.br>
2023-02-21 16:36:56 -0300

    delete a todo

--- src/index.js
+++ src/index.js
@@ -102,7 +102,18 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoDelete = user.todos.find(todo => todo.id === id);
+
+  if (!todoDelete) {
+    return response.status(404).json({ error: "Todo not found!" })
+  }
+
+  user.todos.splice(todoDelete, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
